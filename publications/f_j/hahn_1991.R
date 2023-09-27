979df5c854599e8980e1aeae905ce83abf1812c7
brycefrank <bfrank70@gmail.com>
2023-09-27 09:05:40 -0700

    fix: defining site index base age for hahn_1991 models, closes #20

--- publications/f_j/hahn_1991.R
+++ publications/f_j/hahn_1991.R
@@ -21,15 +21,15 @@ hahn_1991 <- Publication(
 cuvol <- FixedEffectsSet(
   response_unit = list(vsia = units::as_units("ft^3")),
   covariate_units = list(
-    hstix = units::as_units("ft"),
+    hstix50 = units::as_units("ft"),
     dsob = units::as_units("in")
   ),
   parameter_names = c("b_1", "b_2", "b_3", "b_4"),
-  predict_fn = function(hstix, dsob) {
-    b_1 * hstix^b_2 * (1 - exp(b_3 * dsob^b_4))
+  predict_fn = function(hstix50, dsob) {
+    b_1 * hstix50^b_2 * (1 - exp(b_3 * dsob^b_4))
   },
   covariate_definitions = list(
-    hstix = "Site index at an un-specified base age."
+    hstix50 = "Site index at an un-specified base age."
   ),
   model_specifications = load_parameter_frame("vsa_hahn_1991_1")
 )
@@ -37,15 +37,15 @@ cuvol <- FixedEffectsSet(
 bdft <- FixedEffectsSet(
   response_unit = list(vsia = units::as_units("board_foot")),
   covariate_units = list(
-    hstix = units::as_units("ft"),
+    hstix50 = units::as_units("ft"),
     dsob = units::as_units("in")
   ),
   parameter_names = c("b_1", "b_2", "b_3", "b_4"),
-  predict_fn = function(hstix, dsob) {
-    b_1 * hstix^b_2 * (1 - exp(b_3 * dsob^b_4))
+  predict_fn = function(hstix50, dsob) {
+    b_1 * hstix50^b_2 * (1 - exp(b_3 * dsob^b_4))
   },
   covariate_definitions = list(
-    hstix = "Site index at an un-specified base age."
+    hstix50 = "Site index at an un-specified base age."
   ),
   model_specifications = load_parameter_frame("vsa_hahn_1991_2")
 )
