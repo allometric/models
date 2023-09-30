b2f305cd1d6f42a395c7e1c0c0abc2085b9ade6c
brycefrank <bfrank70@gmail.com>
2023-09-30 10:10:12 -0700

    feat(models): added zeide_2002, closes #18

---
+++ publications/u_z/zeide_2002.R
@@ -0,0 +1,45 @@
+zeide_2002 <- Publication(
+  citation = RefManageR::BibEntry(
+    key = "zeide_2002",
+    bibtype = "inproceedings",
+    title = "The effect of density on the height-diameter relationship",
+    author = "Zeide, Boris and Vanderschaaf, Curtis",
+    booktitle = "Proceedings of the 11th Biennial Southern Silvicultural Research Conference",
+    pages = "463--466",
+    year = 2002,
+    organization = "General Technical Report SRS-48. USDA, Forest Service. Southern Research Station"
+  ),
+  descriptors = list(
+    family = "Pinaceae",
+    genus = "Pinus",
+    species = "taeda",
+    country = "US",
+    region = "US-AR"
+  )
+)
+
+normal <- FixedEffectsModel(
+  response_unit = list(
+    hst_s = units::as_units("ft")
+  ),
+  covariate_units = list(
+    dsob_s = units::as_units("in")
+  ),
+  parameters = list(
+    a = 9.4734,
+    b = 0.7374
+  ),
+  predict_fn = function(dsob_s) {
+    a * dsob_s^b
+  },
+  response_definition = "Arithmetic mean height of trees",
+  covariate_definitions = list(
+    dsob_s = "Quadratic mean diameter"
+  ),
+  descriptors = list(
+    model_name = "Normal height-diameter relationship"
+  )
+)
+
+zeide_2002 <- zeide_2002 %>%
+  add_model(normal)
