4d6e45ef4a6005ea643d2db38c8df3c15e132e6b
brycefrank <bfrank70@gmail.com>
2024-09-23 13:05:06 -0700

    adding functions that detect publication changes, updates #69

---
+++ R/file_mod.R
@@ -0,0 +1,21 @@
+parse_parameter_names <- function(filenames) {
+  pattern <- "(?:.*_)?([a-zA-Z]+)_(\\d{4}[a-zA-Z]?)(?:_.*)?\\.csv"
+  matches <- stringr::str_match(filenames, pattern)
+
+  unique(paste0(matches[,2], "_", matches[,3]))
+}
+
+get_modified_files <- function() {
+  files <- system("git diff --name-only HEAD~1 HEAD", intern = TRUE)
+
+  modified_via_params <- files[startsWith(files, "parameters")] |>
+    basename() |>
+    parse_parameter_names()
+
+
+  modified_via_pubs <- files[startsWith(files, "publications")] |>
+    basename() |>
+    tools::file_path_sans_ext()
+
+  unique(c(modified_via_params, modified_via_pubs))
+}
